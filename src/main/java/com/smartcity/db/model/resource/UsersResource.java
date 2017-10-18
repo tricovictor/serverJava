01c58520666a792ce358c5608c835d997aa596fe
Victor Torterola <tricovictor@gmail.com>
2017-10-18 14:13:06 -0300

    Merge branch 'master' of https://github.com/tricovictor/serverJava
    # Conflicts:
    #	src/main/java/com/smartcity/db/model/repository/interfaces/UserController.java

--- src/main/java/com/smartcity/db/model/resource/UsersResource.java
+++ src/main/java/com/smartcity/db/model/resource/UsersResource.java
@@ -1,14 +1,15 @@
 package com.smartcity.db.model.resource;
 
-import com.mysql.fabric.Response;
 import com.smartcity.db.model.Users;
 import com.smartcity.db.model.repository.interfaces.IUsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.List;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @EnableWebMvc
 @RequestMapping(value = "/rest/users")
@@ -24,7 +25,7 @@ public class UsersResource {
 
     @RequestMapping(method = RequestMethod.POST, value = "/add")
     public String persist(@RequestBody Users users) {
-        //iUsersRepository.save(users);
+        iUsersRepository.save(users);
         return "Usuario Creado correctamente";
     }
 

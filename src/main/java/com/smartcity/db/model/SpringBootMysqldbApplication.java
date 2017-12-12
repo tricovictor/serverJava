b28c22cc2849011fb1b71bee9aca3bc78db36e2a
Victor Torterola <tricovictor@gmail.com>
2017-12-12 15:50:56 -0300

    A partir de esta version se genera el war automaticamente para subir al servidor

--- src/main/java/com/smartcity/db/model/SpringBootMysqldbApplication.java
+++ src/main/java/com/smartcity/db/model/SpringBootMysqldbApplication.java
@@ -1,14 +1,27 @@
 package com.smartcity.db.model;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 
 @EnableJpaRepositories(basePackages = "com.smartcity.db.model.repository")
+@EnableAutoConfiguration
+@Component
+@Configuration
 @SpringBootApplication
-public class SpringBootMysqldbApplication {
+public class SpringBootMysqldbApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMysqldbApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootMysqldbApplication.class);
 	}
 }
